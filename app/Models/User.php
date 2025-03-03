5787fad3b0ea5b6079307db01b5ec323339dfda8
Pepista <pcechak07@gmail.com>
2025-03-03 08:35:54 +0100

    funguje

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,47 @@
 <?php
- 
+
 namespace App\Models;
- 
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
- 
-class User extends Authenticatable implements FilamentUser
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
 {
-    // ...
- 
-    public function canAccessPanel(Panel $panel): bool
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
